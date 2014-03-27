c90f8d80ca8722ca7e9eb7ceab93a96c9eb6a033
Kenni Bawden <kenni.bawden@nicta.com.au>
2014-03-27 13:47:49 +1100

    Fixed test code
    Added beforeEach calls instead of assuming things run in order

--- tests/graph.js
+++ tests/graph.js
@@ -4,33 +4,35 @@ var sampleConcept = {
 }
 
 describe('Knowledge graph', function() {
-	var kg;
-
   it('should exist and have a create function', function() {
     expect(knowledgeGraph).toBeDefined();
     expect(knowledgeGraph.create).toBeDefined();
 	
 		describe('Creating a knowledge graph', function() {
 			it('should create a knowledge graph object', function() {
-				kg = knowledgeGraph.create();
+				var kg = knowledgeGraph.create();
 				expect(kg).toBeDefined();
 			});
 		});
   });
 
-
   it('should have a method to create a concept', function() {
     expect(knowledgeGraph.addConcept).toBeDefined();
 
 		describe('Adding a concept to a knowledge graph', function() {
-			kg.addConcept({concept: sampleConcept});
+			var kg;
+
+			beforeEach(function() {
+				kg = knowledgeGraph.create();
+		  	kg.addConcept({concept: sampleConcept});
+			});
 
 			it('should add a new node', function() {
 				expect(kg.graph.node(sampleConcept.id)).toBeDefined();
 			});
 
 			it('should display the new node', function() {
-				var node = kg.element.select('.node.'+sampleConcept.id).node();
+				var node = kg.element.select('.node#'+sampleConcept.id).node();
 				expect(node).not.toBe(null);
 			});
 		});
