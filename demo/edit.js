50a8178f3fdca2715462bc4727b0013bfc904bde
Kenni Bawden <kenni.bawden@nicta.com.au>
2014-04-11 16:20:14 +1000

    Added content to each node in the edit example

--- demo/edit.js
+++ demo/edit.js
@@ -4,27 +4,42 @@ var knowledge = {
   concepts: [
     {
       id: "edit",
-      name: "Click here!"
+      name: "Click here!",
+      content: [{
+        text: "Click on a concept and edit its name and contents or remove it completely."
+      }]
     },
     {
       id: "dependency",
       name: "Click the top",
-      dependencies: ["edit"]
+      dependencies: ["edit"],
+      content: [{
+        text: "Click the top semi-circle of this concept to add a new concept that this concept depends on. Click on the new concept to edit its title and contents."
+      }]
     },
     {
       id: "dependent",
       name: "Click the bottom",
-      dependencies: ["edit"]
+      dependencies: ["edit"],
+      content: [{
+        text: "Click the bottom semi-circle of this concept to add a new concept that follows this concept. The new concept depends on this concept. Click on the new concept to edit its title and contents."
+      }]
     },
     {
       id: "joining",
       name: "Drag between disconnected concepts",
-      dependencies: ["edit"]
+      dependencies: ["edit"],
+      content: [{
+        text: "Drag from the top semi-circle of this concept to another concept to add the other concept as a dependency to this concept. Drag from the bottom semi-circle of this concept to another concept to add this concept as a dependency of the other concept."
+      }]
     },
     {
       id: "removing",
       name: "Drag between connected concepts",
-      dependencies: ["joining"]
+      dependencies: ["joining"],
+      content: [{
+        text: "Drag between two connected concepts to remove the connection."
+      }]
     }
   ]
 };
