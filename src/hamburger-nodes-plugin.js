73e93056c05a2f825722eda55a2094b1ef9a66cd
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-08-07 03:17:17 +0000

    Update edge positioning with burger buns.

--- src/hamburger-nodes-plugin.js
+++ src/hamburger-nodes-plugin.js
@@ -11,11 +11,7 @@ var setupHamburgerNodes = function(km) {
     // Add enter/above
     nodes.filter('.resource').insert('path', 'rect')
       .attr('d', semicircle)
-      .classed('enter', true)
-      .attr('transform', function(n) {
-        var height = getTextHeight.call(this);
-        return 'translate(0,' + (-height) + ')';
-      });
+      .classed('enter', true);
 
     // Flip the semi-circle
     semicircle
@@ -25,16 +21,25 @@ var setupHamburgerNodes = function(km) {
     // Add exit/below
     nodes.filter('.resource').insert('path', 'rect')
       .attr('d', semicircle)
-      .classed('exit', true)
-      .attr('transform', function(n) {
-        var height = getTextHeight.call(this);
-        return 'translate(0,' + (height) + ')';
-      });
+      .classed('exit', true);
   });
 
   function getTextHeight() {
     return d3.select(this.parentNode).select('text').node().getBBox().height;
   }
+
+  km.positionNodes.onUpdate(function(nodes) {
+    nodes.select('path.enter')
+      .attr('transform', function(n) {
+          var textHeight = getTextHeight.call(this);
+          return 'translate(0,' + (-textHeight/2-2) + ')';
+        });
+    nodes.select('path.exit')
+      .attr('transform', function(n) {
+          var textHeight = getTextHeight.call(this);
+          return 'translate(0,' + (textHeight/2+2) + ')';
+        });
+  });
 
   function nodePosition(node, point) {
     var x = node.x;
@@ -56,18 +61,41 @@ var setupHamburgerNodes = function(km) {
     return {x: x + dxu*r, y: y + offset + dyu*r}; 
   }
 
-  km.positionNodes.onUpdate(function(nodes) {
-    nodes.select('path.enter')
-      .attr('transform', function(n) {
-          var textHeight = getTextHeight.call(this);
-          return 'translate(0,' + (-textHeight/2-2) + ')';
-        });
-    nodes.select('path.exit')
-      .attr('transform', function(n) {
-          var textHeight = getTextHeight.call(this);
-          return 'translate(0,' + (textHeight/2+2) + ')';
-        });
-  });
+  /*
+  Rectangle intersection from dagre-d3 source.
+  */
+  function intersectRect(rect, point) {
+    var x = rect.x;
+    var y = rect.y;
+
+    // For now we only support rectangles
+
+    // Rectangle intersection algorithm from:
+    // http://math.stackexchange.com/questions/108113/find-edge-between-two-boxes
+    var dx = point.x - x;
+    var dy = point.y - y;
+    var w = rect.width / 2 + 5;
+    var h = rect.height / 2 + 5;
+
+    var sx, sy;
+    if (Math.abs(dy) * w > Math.abs(dx) * h) {
+      // Intersection is top or bottom of rect.
+      if (dy < 0) {
+        h = -h;
+      }
+      sx = dy === 0 ? 0 : h * dx / dy;
+      sy = h;
+    } else {
+      // Intersection is left or right of rect.
+      if (dx < 0) {
+        w = -w;
+      }
+      sx = w;
+      sy = dx === 0 ? 0 : w * dy / dx;
+    }
+
+    return {x: x + sx, y: y + sy};
+  }
 
   km.positionEdges.offUpdate(km.defaultUpdateEdgePositions);
   km.positionEdges.onUpdate(function(edges) {
@@ -80,8 +108,10 @@ var setupHamburgerNodes = function(km) {
         var p0 = path.length === 0 ? target : path[0];
         var p1 = path.length === 0 ? source : path[path.length - 1];
 
-        path.unshift(nodePosition(source, p0));
-        path.push(nodePosition(target, p1));
+        var f0 = e.source.type === 'concept' ? intersectRect : nodePosition;
+        var f1 = e.target.type === 'concept' ? intersectRect : nodePosition;
+        path.unshift(f0(source, p0));
+        path.push(f1(target, p1));
 
         return d3.svg.line()
           .x(function(d) { return d.x; })
